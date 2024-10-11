18c2eeed83ca936f001281b2753f36cac5a90443
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2024-10-11 21:59:58 +0100

    GameManager singleton Started populating player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,17 @@
-class_name Player extends Node3D
+class_name Player extends CharacterBody3D
 
+@export var _camera : Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_do_locomotion(delta)
+	_do_camera_look(delta)
+	
+func _do_locomotion(delta: float) -> void:
+	pass
+	
+func _do_camera_look(delta: float) -> void:
 	pass
